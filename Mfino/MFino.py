9b05f08bbfc4d80d377d65d48d75cdcb804c82d5
SomunsLee <41895712+SomunsLee@users.noreply.github.com>
2021-12-03 15:18:43 +0800

    update

--- Mfino/MFino.py
+++ Mfino/MFino.py
@@ -1,8 +1,6 @@
 import socket
 import mFinoUtils
-
-IP = "10.17.85.22"
-Port = 8583
+from sys import exit
 
 # Payment (Topup)
 # msg = mFinoUtils.getMsgForPayment("88295290170","180000","5011","14","5000")
@@ -17,50 +15,62 @@ Port = 8583
 # Package Purchase
 # msg = mFinoUtils.getMsgForPackagePurchase("88295290170", "490000", "5011", "14", "20000", "329067", "358")
 
-def Judgeinput(var):
-    if var == 0 :
+
+def judgeinput(var):
+    if var == 0:
         exit()
-    if var > 0 and var < 7 :
+    if 0 < var < 8:
         run(var)
     else:
         print("Input error!")
         init()
 
+
 def run(var):
+    msg = "NULL"
     if var == 1:
-        MobileNumber = input("MobileNumber:")
-        Amount = input("Amount:")
-        msg = mFinoUtils.getMsgForPayment(MobileNumber, "180000", "5011", "14", Amount)
+        mobile_number = input("MobileNumber:")
+        amount = input("Amount:")
+        msg = mFinoUtils.get_msg_for_payment(mobile_number, "180000", "0006", "881", amount)
     elif var == 2:
-        MobileNumber = input("MobileNumber:")
-        msg = mFinoUtils.getMsgForInquiry(MobileNumber, "380000", "5011", "14")
+        mobile_number = input("MobileNumber:")
+        msg = mFinoUtils.get_msg_for_inquiry(mobile_number, "380000", "5011", "14")
     elif var == 3:
-        MobileNumber = input("MobileNumber:")
-        Amount = input("Amount:")
-        msg = mFinoUtils.getMsgForPayment(MobileNumber, "280000", "5011", "14", Amount)
+        mobile_number = input("MobileNumber:")
+        amount = input("Amount:")
+        msg = mFinoUtils.get_msg_for_payment(mobile_number, "280000", "5011", "14", amount)
     elif var == 4:
-        SourceMobileNumber = input("SourceMobileNumber:")
-        DestinationMobileNumber = input("DestinationMobileNumber:")
-        Amount = input("Amount:")
-        msg = mFinoUtils.getMsgForTransfer(SourceMobileNumber, "400000", "5011", "14", Amount, DestinationMobileNumber)
+        source_mobile_number = input("SourceMobileNumber:")
+        destination_mobile_number = input("DestinationMobileNumber:")
+        amount = input("Amount:")
+        msg = mFinoUtils.get_msg_for_transfer(source_mobile_number, "400000", "5011", "14", amount,
+                                              destination_mobile_number)
     elif var == 5:
-        MobileNumber = input("MobileNumber:")
-        PackageID = input("PackageID:")
-        AreaID = input("AreaID:")
-        msg = mFinoUtils.getMsgForPackageInquiry(MobileNumber, "390000", "5011", "14", PackageID, AreaID)
+        mobile_number = input("MobileNumber:")
+        package_id = input("PackageID:")
+        area_id = input("AreaID:")
+        msg = mFinoUtils.get_msg_for_package_inquiry(mobile_number, "390000", "5011", "14", package_id, area_id)
     elif var == 6:
-        MobileNumber = input("MobileNumber:")
-        Amount = input("Amount:")
-        PackageID = input("PackageID:")
-        AreaID = input("AreaID:")
-        msg = mFinoUtils.getMsgForPackagePurchase(MobileNumber, "490000", "5011", "14", Amount, PackageID, AreaID)
+        mobile_number = input("MobileNumber:")
+        amount = input("Amount:")
+        package_id = input("PackageID:")
+        area_id = input("AreaID:")
+        msg = mFinoUtils.get_msg_for_package_purchase(mobile_number, "490000", "5011", "14", amount, package_id,
+                                                      area_id)
+    elif var == 7:
+        mobile_number = input("MobileNumber:")
+        amount = input("Amount:")
+        package_id = input("PackageID:")
+        city_code = input("CityCode:")
+        msg = mFinoUtils.get_msg_for_package_purchase_area(mobile_number, "590000", "5011", "14", amount, package_id,
+                                                           city_code)
     print(msg)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((IP, Port))
+        s.connect((ip, int(port)))
         s.sendall(msg.encode())
         date = s.recv(1024)
         print(date.decode())
-        init()
+    init()
 
 
 def init():
@@ -72,8 +82,13 @@ def init():
     print("4: Share Load / Transfer balance")
     print("5: Package Inquiry")
     print("6: Package Purchase")
-    Judgeinput(int(input("Select:")))
+    print("7: Package Purchase Area")
+    judgeinput(int(input("Select:")))
+
 
 if __name__ == "__main__":
-    print("Welcome to use simulation tool for mFino.")
+    print("Welcome To Use Simulation Tool For MFino Testing.")
+    print("Prohibit The Use Of This Tool In Production Systems")
+    ip = input("Server IP:")
+    port = input("Server Port:")
     init()
--- Mfino/mFinoUtils.py
+++ Mfino/mFinoUtils.py
@@ -2,125 +2,144 @@ import uuid
 from datetime import datetime
 
 
-def getBitMap(processingCode):
-    if processingCode in ('380000', '390000'):
+def get_bit_map(processing_code):
+    if processing_code in ('380000', '390000'):
         return "0200223A400108218008"
-    elif processingCode in ('180000', '280000', '490000'):
+    elif processing_code in ('180000', '280000', '490000', '590000'):
         return "0200323A400108A18008"
-    elif processingCode in ('400000'):
+    elif processing_code in '400000':
         return "02003238400108010008"
     else:
         return "NULL"
 
 
-def getTransactionAmount(Amount):
-    return ("000000000000" + Amount)[-12:]
+def get_transaction_amount(amount):
+    return ("000000000000" + amount)[-12:]
 
 
-def getTransmissionTime():
+def get_transmission_time():
     return datetime.now().strftime('%m%d%H%M%S')
 
 
-def getSystemTraceAuditNumber():
+def get_system_trace_audit_number():
     return datetime.now().strftime('%f')
 
 
-def getLocalTransactionTime():
+def get_local_transaction_time():
     return datetime.now().strftime('%H%M%S')
 
 
-def getLocalTransactionDate():
+def get_local_transaction_date():
     return datetime.now().strftime('%m%d')
 
 
-def getSettlementDate():
+def get_settlement_date():
     return datetime.now().strftime('%m%d')
 
 
-def getBankCode(bankCode):
-    if len(bankCode) <= 3:
-        return "03" + ("000" + bankCode)[-3:]
-    elif len(bankCode) < 10:
-        return "0" + str(len(bankCode)) + bankCode
+def get_bank_code(bank_code):
+    if len(bank_code) <= 3:
+        return "03" + ("000" + bank_code)[-3:]
+    elif len(bank_code) < 10:
+        return "0" + str(len(bank_code)) + bank_code
     else:
         return "03881"
 
 
-def getRetrievalReferenceNumber():
+def get_retrieval_reference_number():
     return uuid.UUID(int=uuid.getnode()).hex[-12:]
 
 
-def getCardAcceptorTerminalIdentification():
+def get_card_acceptor_terminal_identification():
     return "11      "
 
 
-def getCardAcceptorName():
+def get_card_acceptor_name():
     return "SMS SMART                               "
 
 
-def getProductIndicator():
+def get_product_indicator():
     return "5100"
 
 
-def getCurrenyCode():
+def get_currency_code():
     return "360"
 
 
-def getMDN(MDN):
-    return ("62" + MDN + "                ")[0:16]
+def get_mdn(mdn):
+    return ("62" + mdn + "                ")[0:16]
 
 
-def getPackageID(packageID):
-    return ("000000000000" + packageID)[-12:]
+def get_package_id(package_id):
+    return ("000000000000" + package_id)[-12:]
 
 
-def getAreaID(areaID):
-    return (areaID + "         ")[0:9]
+def get_area_id(area_id):
+    return (area_id + "         ")[0:9]
 
 
-def getMsgForInquiry(mobileNumber, processingCode, merchantCode, bankCode):
-    msg = getBitMap(
-        processingCode) + processingCode + getTransmissionTime() + getSystemTraceAuditNumber() + getLocalTransactionTime() + getLocalTransactionDate() + getSettlementDate() + merchantCode + getBankCode(
-        bankCode) + getRetrievalReferenceNumber() + getCardAcceptorName() + getProductIndicator() + getCurrenyCode() + "016" + getMDN(
-        mobileNumber)
+def get_city_code(city_code):
+    return (city_code + "                                                  ")[0:50]
+
+
+def get_msg_for_inquiry(mobile_number, processing_code, merchant_code, bank_code):
+    msg = get_bit_map(
+        processing_code) + processing_code + get_transmission_time() + get_system_trace_audit_number() + \
+          get_local_transaction_time() + get_local_transaction_date() + get_settlement_date() + merchant_code + \
+          get_bank_code(bank_code) + get_retrieval_reference_number() + get_card_acceptor_name() + \
+          get_product_indicator() + get_currency_code() + "016" + get_mdn(mobile_number)
     msg = ("0" * 4 + str(len(msg)))[-4:] + msg
     return msg
 
 
-def getMsgForPayment(mobileNumber, processingCode, merchantCode, bankCode, transactionAmount):
-    msg = getBitMap(
-        processingCode) + processingCode + getTransactionAmount(
-        transactionAmount) + getTransmissionTime() + getSystemTraceAuditNumber() + getLocalTransactionTime() + getLocalTransactionDate() + getSettlementDate() + merchantCode + getBankCode(
-        bankCode) + getRetrievalReferenceNumber() + getCardAcceptorTerminalIdentification() + getCardAcceptorName() + getProductIndicator() + getCurrenyCode() + "016" + getMDN(
-        mobileNumber)
+def get_msg_for_payment(mobile_number, processing_code, merchant_code, bank_code, transaction_amount):
+    msg = get_bit_map(processing_code) + processing_code + get_transaction_amount(transaction_amount) + \
+          get_transmission_time() + get_system_trace_audit_number() + get_local_transaction_time() + \
+          get_local_transaction_date() + get_settlement_date() + merchant_code + get_bank_code(bank_code) + \
+          get_retrieval_reference_number() + get_card_acceptor_terminal_identification() + get_card_acceptor_name() + \
+          get_product_indicator() + get_currency_code() + "016" + get_mdn(mobile_number)
     msg = ("0" * 4 + str(len(msg)))[-4:] + msg
     return msg
 
 
-def getMsgForTransfer(mobileNumber, processingCode, merchantCode, bankCode, transactionAmount, mobileNumberB):
-    msg = getBitMap(
-        processingCode) + processingCode + getTransactionAmount(
-        transactionAmount) + getTransmissionTime() + getSystemTraceAuditNumber() + getLocalTransactionTime() + getLocalTransactionDate() + merchantCode + getBankCode(
-        bankCode) + getRetrievalReferenceNumber() + getProductIndicator() + "032" + getMDN(
-        mobileNumber) + getMDN(mobileNumberB)
+def get_msg_for_transfer(mobile_number, processing_code, merchant_code, bank_code, transaction_amount, mobile_number_b):
+    msg = get_bit_map(processing_code) + processing_code + get_transaction_amount(transaction_amount) + \
+          get_transmission_time() + get_system_trace_audit_number() + get_local_transaction_time() + \
+          get_local_transaction_date() + merchant_code + get_bank_code(bank_code) + get_retrieval_reference_number() + \
+          get_product_indicator() + "032" + get_mdn(mobile_number) + get_mdn(mobile_number_b)
     msg = ("0" * 4 + str(len(msg)))[-4:] + msg
     return msg
 
 
-def getMsgForPackageInquiry(mobileNumber, processingCode, merchantCode, bankCode, packageID, areaID):
-    msg = getBitMap(
-        processingCode) + processingCode + getTransmissionTime() + getSystemTraceAuditNumber() + getLocalTransactionTime() + getLocalTransactionDate() + getSettlementDate() + merchantCode + getBankCode(
-        bankCode) + getRetrievalReferenceNumber() + getCardAcceptorName() + getProductIndicator() + getCurrenyCode() + "037" + getMDN(
-        mobileNumber) + getPackageID(packageID) + getAreaID(areaID)
+def get_msg_for_package_inquiry(mobile_number, processing_code, merchant_code, bank_code, package_id, area_id):
+    msg = get_bit_map(processing_code) + processing_code + get_transmission_time() + get_system_trace_audit_number() + \
+          get_local_transaction_time() + get_local_transaction_date() + get_settlement_date() + merchant_code + \
+          get_bank_code(bank_code) + get_retrieval_reference_number() + get_card_acceptor_name() + \
+          get_product_indicator() + get_currency_code() + "037" + get_mdn(mobile_number) + \
+          get_package_id(package_id) + get_area_id(area_id)
     msg = ("0" * 4 + str(len(msg)))[-4:] + msg
     return msg
 
 
-def getMsgForPackagePurchase(mobileNumber, processingCode, merchantCode, bankCode, transactionAmount, packageID, areaID):
-    msg = getBitMap(
-        processingCode) + processingCode + getTransactionAmount(
-        transactionAmount) + getTransmissionTime() + getSystemTraceAuditNumber() + getLocalTransactionTime() + getLocalTransactionDate() + getSettlementDate() + merchantCode + getBankCode(
-        bankCode) + getRetrievalReferenceNumber() + getCardAcceptorTerminalIdentification() + getCardAcceptorName() + getProductIndicator() + getCurrenyCode() + "037" + getMDN(
-        mobileNumber) + getPackageID(packageID) + getAreaID(areaID)
+def get_msg_for_package_purchase(mobile_number, processing_code, merchant_code, bank_code, transaction_amount,
+                                 package_id, area_id):
+    msg = get_bit_map(processing_code) + processing_code + get_transaction_amount(transaction_amount) + \
+          get_transmission_time() + get_system_trace_audit_number() + get_local_transaction_time() + \
+          get_local_transaction_date() + get_settlement_date() + merchant_code + get_bank_code(bank_code) + \
+          get_retrieval_reference_number() + get_card_acceptor_terminal_identification() + get_card_acceptor_name() + \
+          get_product_indicator() + get_currency_code() + "037" + get_mdn(mobile_number) + \
+          get_package_id(package_id) + get_area_id(area_id)
+    msg = ("0" * 4 + str(len(msg)))[-4:] + msg
+    return msg
+
+
+def get_msg_for_package_purchase_area(mobile_number, processing_code, merchant_code, bank_code, transaction_amount,
+                                      package_id, city_code):
+    msg = get_bit_map(processing_code) + processing_code + get_transaction_amount(transaction_amount) + \
+          get_transmission_time() + get_system_trace_audit_number() + get_local_transaction_time() + \
+          get_local_transaction_date() + get_settlement_date() + merchant_code + get_bank_code(bank_code) + \
+          get_retrieval_reference_number() + get_card_acceptor_terminal_identification() + get_card_acceptor_name() + \
+          get_product_indicator() + get_currency_code() + "078" + get_mdn(mobile_number) + \
+          get_package_id(package_id) + get_city_code(city_code)
     msg = ("0" * 4 + str(len(msg)))[-4:] + msg
     return msg
